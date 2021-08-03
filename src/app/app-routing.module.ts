3e961fe427f2b15320af2f88a301acb3995d38ed
Jonathan <jonulisanpar@gmail.com>
2021-08-03 10:30:10 -0500

    Seccion 12: HeroesApp - Rutas hijas y Lazyload
    Capitulo 173: Rutas hijas y lazyload - AuthRoutes
    En este capitulo se aprendio a hacer rutas hijas, que seran desplegadas
    desde una ruta Root, en esto se basa el lazyload, que es cargar los
    modulos bajo demanda.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,13 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
   {
+    path: 'auth',
+    //Para el Lazyload y rutas hijas, se especifica que cargue estas rutas. Esto se hace
+    //mediante una lamba. Pedira importar un modulo, en este caso auth.module. Cuendo
+    //este este cargado, regresara el modulo de Auth.
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+  },
+  {
     path: '404',
     component: ErrorPageComponent
   },
---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+
+const routes: Routes = [
+  {
+    //Como se especifica en las rutas padre, que ruta se debe de tener para acceder a
+    //estas rutas hijas, se puede poder este path como un path vacio.
+    path: '',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'registro',
+        component: RegistroComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'login'
+      }
+    ]
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forChild( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AuthRoutingModule { }
