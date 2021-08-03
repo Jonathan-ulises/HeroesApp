6a18188bd4508b5ef155c0ec789fa02072058016
Jonathan <jonulisanpar@gmail.com>
2021-08-03 17:31:31 -0500

    Seccion 13: HeroesApp - Angular Material y Angular Flex
    Capitulo 194: Variables de entorno
    En este capitulo se aprendio que son los enviroments, y la gran utilidad
    que estas dan al momento de desplegar o desarrollar aplicaciones.

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,19 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Heroe } from '../interfaces/heroes.interfaces';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  //Envirament del servicio: desarrollo
+  private baseURL: string = environment.baseURL;
+
   constructor( private http: HttpClient ) { }
 
   getHeroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${ this.baseURL }/heroes`);
   }
 
   getHeroePorId( id: string ): Observable<Heroe> {
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${ id }`);
+    return this.http.get<Heroe>(`${ this.baseURL }/heroes/${ id }`);
   }
 }
