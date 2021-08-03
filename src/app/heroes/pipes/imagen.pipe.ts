18232625c247ab6474ad17cc08f6b13a76a4aa5e
Jonathan <jonulisanpar@gmail.com>
2021-08-03 16:25:19 -0500

    Seccion 13: HeroesApp - Angular Material y Angular flex
    Capitulo 190: Tarea - PipeImagen
    En este capitulo se realizo la practica de hacer un pipe que con el id
    del heroe, generara la ruta para que la imagen pueda ser mostrada en la
    aplicacion.

---
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Heroe } from '../interfaces/heroes.interfaces';
+
+@Pipe({
+  name: 'imagen'
+})
+export class ImagenPipe implements PipeTransform {
+
+  transform(value: Heroe): string {
+    return (value.alt_img  !== '') ? `assets/heroes/${value.id}.jpg` : 'assets/no-imagen.png';
+  }
+
+}
