f0c7e9666988fa0880585b16d25993ab34eb7f61
Jonathan <jonulisanpar@gmail.com>
2021-08-03 13:40:43 -0500

    Seccion 13: HeroesApp - Angular material y Angular flex
    Capitulo 185: Heroes Service - Traer informacion de los heroes
    En este capitulo se creo el servicio para consumir los datos de heroes.

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
@@ -8,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+  constructor( private heroesServie: HeroesService) { }
 
   ngOnInit(): void {
+    this.heroesServie.getHeroes()
+      .subscribe( resp => console.log( resp ));
   }
 
 }
