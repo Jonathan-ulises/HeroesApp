3431fa5a5a697cf47602c75787e0c721358c2797
Jonathan <jonulisanpar@gmail.com>
2021-08-04 14:23:13 -0500

    Seccion 14: HeroesApp - CRUD (Continuacion Angular Material)
    Capitulo 211: Material Dialog
    En este capitulo se vio como usar el dialog  de material desing, ese
    basicamente se basa en el diseÃo de un componente individual. Solo falta
    la comunicacion de informacion entre el componente de editar y el
    componente para el dialogo.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button'; 
 import { MatCardModule } from '@angular/material/card'; 
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list'; 
 import { MatIconModule } from '@angular/material/icon'; 
@@ -27,6 +28,7 @@ MatInputModule
     MatAutocompleteModule,
     MatButtonModule,
     MatCardModule,
+    MatDialogModule,
     MatFormFieldModule,
     MatGridListModule,
     MatIconModule,
