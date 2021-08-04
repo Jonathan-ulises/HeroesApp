9cd7fbe8bc7bdcf18832c2a9b39413983d58f544
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-08-03 23:17:44 -0500

    Seccion 13: HeroesApp - Angular Material y Angular Flex
    Capitulo 196: Autocomplete - Segunda parte
    En este capitulo se termino el autocompletado; ahora falta realzar
    excepciones para los casos de valores no encontrados por la consulta de
    las sugerencias.

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete/autocomplete';
 import { Heroe } from '../../interfaces/heroes.interfaces';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -12,6 +13,7 @@ export class BuscarComponent implements OnInit {
 
   termino: string = '';
   heroes: Heroe[] = [];
+  heroeSeleccionado!: Heroe;
 
   constructor(
     private heroesService: HeroesService
@@ -20,11 +22,25 @@ export class BuscarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /**
+   * Metodo para buscar las sugerencias del autocomplete
+   */
   buscando(): void {
-    this.heroesService.getHeroes()
+    this.heroesService.getSugerencias( this.termino )
       .subscribe( (resp) => {
         this.heroes = resp;
       })
+  }
+
+  /**
+   * Optiene el elemento seleccionado y realiza una consulta al backEnd.
+   * @param event evento de la opcion seleccionada
+   */
+  opcionSeleccionada( event: MatAutocompleteSelectedEvent ) : void {
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+    this.heroesService.getHeroePorId( heroe.id! )
+      .subscribe( heroe => this.heroeSeleccionado = heroe);
   }
 
 }
