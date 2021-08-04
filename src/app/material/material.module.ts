a9b82f0ce0894d756fa87dfa7382d2e3c6ae1ebf
Jonathan <jonulisanpar@gmail.com>
2021-08-04 14:55:16 -0500

    Seccion 14: HeroesApp - CRUD (Continuacion Angular Material)
    Capitulo 213: Adecuar los textos de la pantalla de agregar
    En este capitulo se cambio el titulo del AgregarComponent y los botones
    del formulario por texto dinamico; el texto mostrado es acode a la
    seccion que pertenece. Tambien se le dio una limpieza al modulo de
    material, se le agrego espaciado a los imports para que sean agradables
    a la vista.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatButtonModule } from '@angular/material/button'; 
-import { MatCardModule } from '@angular/material/card'; 
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list'; 
-import { MatIconModule } from '@angular/material/icon'; 
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list'; 
+import { MatAutocompleteModule }    from '@angular/material/autocomplete';
+import { MatButtonModule }          from '@angular/material/button'; 
+import { MatCardModule }            from '@angular/material/card'; 
+import { MatDialogModule }          from '@angular/material/dialog';
+import { MatFormFieldModule }       from '@angular/material/form-field';
+import { MatGridListModule }        from '@angular/material/grid-list'; 
+import { MatIconModule }            from '@angular/material/icon'; 
+import { MatInputModule }           from '@angular/material/input';
+import { MatListModule }            from '@angular/material/list'; 
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSelectModule } from '@angular/material/select'; 
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule }          from '@angular/material/select'; 
+import { MatSidenavModule }         from '@angular/material/sidenav';
+import { MatSnackBarModule }        from '@angular/material/snack-bar';
+import { MatToolbarModule }         from '@angular/material/toolbar';
 
 /*
 Modulos necesario para el autocomplete:
