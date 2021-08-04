c0663e68eeea82a060287c020a473722c3205732
Jonathan <jonulisanpar@gmail.com>
2021-08-04 13:54:27 -0500

    Seccion 14: HeroesApp - CRUD (Continuacion Angular Material)
    Capitulo 209: Pipes Puros e Impuros
    En este capitulo se vio que es un pipe inpuro y un puro. Los impuros se
    ejecutan cuando el sistema de deteccion de cambios de angular es
    ejectuado. Si es puro, solo se ejecuta cuando se cree el componente en
    la aplicacion.

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -2,11 +2,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Heroe } from '../interfaces/heroes.interfaces';
 
 @Pipe({
-  name: 'imagen'
+  name: 'imagen',
+  //Si es falso, se ejecuta el pipe con el detector de cambios de angular
+  //pure: false
 })
 export class ImagenPipe implements PipeTransform {
 
   transform( heroe: Heroe): string {
+
+    //console.log('Pipe imagen se proceso');
 
     if ( !heroe.id && !heroe.alt_img ) {
       return 'assets/no-image.png';
