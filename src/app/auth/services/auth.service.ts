f23003d175d4db5ac656fd8be85fd330a87161a1
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-08-15 15:30:07 -0500

    Seccion 15: Proteccion de Rutas
    Capitulo 220: Mostrar la informacion del usuario activo
    En este capitulo se logro mostrar el nombre del usuario que fue logeado,
    esta se guarda en una propiedad creada en el servicio auth, esta se
    llena de informacion gracias al operador tap que podemos usar en un pipe
    al crear el servicio. este tap ejecuta una accion secuencial cuando se
    ejecuta una principalmente.

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { Auth } from '../interfaces/auth.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,20 @@ import { Auth } from '../interfaces/auth.interface';
 export class AuthService {
 
   private baseurl: string = environment.baseURL;
+  private _auth: Auth | undefined;
+
+  get auth() {
+    return {...this._auth};
+  }
 
   constructor(
     private http: HttpClient
   ) { }
 
   login() : Observable<Auth> {
-    return this.http.get<Auth>(`${ this.baseurl }/usuarios/1`);
+    return this.http.get<Auth>(`${ this.baseurl }/usuarios/1`)
+      .pipe(
+        tap( auth => this._auth = auth )
+      );
   }
 }
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Auth } from 'src/app/auth/interfaces/auth.interface';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -14,8 +16,13 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
 
+  get auth() {
+    return this.authService.auth;
+  }
+
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   logout() : void {
